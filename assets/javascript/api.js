0acba5f6ade895eb0b02851fa31ffa65bf5b895a
Curtis Dev K <curt_king@coolsite.net>
2023-08-29 22:21:42 +0100

    have updated color scheme and added 3d effect to heading, also added further responsiveness

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,5 +1,7 @@
 import { apiKey } from './config.js';
 
+export let movieArray;
+
 export async function getMovies() {
     const url = 'https://moviesdatabase.p.rapidapi.com/titles/random?list=most_pop_movies&genre=Action&info=base_info';
     const options = {
@@ -13,9 +15,20 @@ export async function getMovies() {
     try {
         const response = await fetch(url, ...[options]);
         const data = await response.json();
-        console.log(data);
+        return movieArray = data.results;
     } catch (error) {
         console.error(error);
+        throw new Error('Failed to fetch movies ' + error.message);
     }
 }
 
+
+// function to populate movieArray 
+
+export const populateMovieArray = async () => {
+    try {
+        movieArray = await getMovies();
+    } catch (error) {
+        console.log(error);
+    }
+}
--- assets/javascript/moviePicker.js
+++ assets/javascript/moviePicker.js
@@ -1,6 +1,82 @@
-import { getMovies } from "./api.js";
+import { getMovies, movieArray } from "./api.js";
 
-getMovies();
+async function initialize() {
+    await getMovies();
+    printOut(movieArray);
+}
+
+ let printOut = async (list) => {
+    list.forEach(element => {
+        console.log(element);
+        // call renderMovies function for each element
+        renderMovies(element);
+    });
+}
+
+initialize();
+
+
+// function to create required html elements for each movie
+
+function renderMovies(movie) {
+    // Create a single film wrapper div
+    const singleFilmWrapper = document.createElement('div');
+    singleFilmWrapper.classList.add('single-film-wrapper');
+
+    // Create a single film inner wrapper div
+    const singleFilmInnerWrapper = document.createElement('div');
+    singleFilmInnerWrapper.classList.add('single-film-inner-wrapper');
+
+    // Create an image element
+    const image = document.createElement('img');
+    image.src = `${movie.primaryImage['url']}`;
+    image.alt = `Title of movie: ${movie.titleText['text']}`;
+    image.classList.add('movie-pic-small');
+
+    // Create an h5 element for the film title
+    const h5 = document.createElement('h5');
+    h5.textContent = movie.titleText['text']; 
+
+    // Create a p element for the movie year
+    const p = document.createElement('p');
+    p.textContent = movie.releaseYear['year'];
+    p.classList.add('movie-year');
+
+    // create a p element for the film description
+    const paraDescription = document.createElement('p');
+    paraDescription.textContent = movie.plot['plotText']['plainText'];
+    paraDescription.classList.add('film-description');
+
+    // create paragraph for rating
+    const rating = document.createElement('p');
+    rating.textContent = "IMDb Rating: " + movie.ratingsSummary['aggregateRating'] + "\nNumber of votes: " + movie.ratingsSummary['voteCount'];
+    rating.classList.add('rating-text');
+
+    // Append the elements to the DOM hierarchy
+    singleFilmInnerWrapper.appendChild(image);
+    singleFilmInnerWrapper.appendChild(h5);
+    singleFilmInnerWrapper.appendChild(p);
+    singleFilmInnerWrapper.appendChild(paraDescription);
+    singleFilmInnerWrapper.appendChild(rating);
+
+    singleFilmWrapper.appendChild(singleFilmInnerWrapper);
+
+    // Append the entire single film wrapper to a container element (e.g., body)
+    const container = document.querySelector('.random-movie-set'); // Replace with your container selector
+    container.appendChild(singleFilmWrapper);
+
+
+}
+
+{/* <div class="single-film-wrapper">
+                <div class="single-film-inner-wrapper">
+                    <img src="./assets/pictures/testing-image.jpg" alt="" class="movie-pic-small">
+                    <h5>Hellraiser 2: Revenge</h5>
+             <p class="movie-year">2018</p>
+       </div>
+</div> */}
+
+
 
 
 // array of objects will be likely approach to handling user input
@@ -18,3 +94,5 @@ getMovies();
 //     },
 //     // more form submissions...
 //   ];
+
+// titleText.text
